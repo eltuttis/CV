72b2c9919c0f86dd38b595ba49b6b6294c27bbe8
eltuttis <jrodriguezgiraldo8@gmail.com>
2023-12-28 01:28:06 -0500

    problema del margin-left resuelto

--- jQuery.js
+++ jQuery.js
@@ -7,17 +7,16 @@ function getRandomInt() {
 function addElement() {
     for (i = 1; i <= 10; i++){
         let div = document.createElement('div');
-        div.classList.add('burbuja'+i);
+        div.classList.add('card_' + i);
         
         //AÑADE EL DIV ANTES DEL TEXTO
         /*var burbujas = document.getElementById("burbujas");
         document.body.insertAdjacentHTML(div,burbujas);*/
-        j=getRandomInt();
         //AÑADE EL DIV DESPUES DEL TEXTO
         document.getElementById("burbujas").appendChild(div);
-        $('.burbuja'+i).css("margin-left", `${j}%`)
+        $('.card_' + i).css("margin-left", `${getRandomInt()}%`)
 
-        console.log(j)
+        console.log()
      }
 }        
 
